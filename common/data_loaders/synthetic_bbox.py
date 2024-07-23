105d60fce9de88c2106d69c5ceed80aeb48d6f35
FranciscoMColino <francisco.m.colino@gmail.com>
2024-07-23 18:41:11 +0100

    Add data loader for synthetic 3d bbox extracted data

--- common/data_loaders/synthetic_bbox.py
+++ common/data_loaders/synthetic_bbox.py
@@ -1,1 +1,109 @@
+import os
+import numpy as np
+import json
+import open3d as o3d
 
+class SyntheticBbox():
+    def __init__(self, data_path, int_precision=4, 
+                 singl_sem_classes=None, translate_vector=None, rotation_matrix=None):
+        self.data_path = data_path
+        self.int_precision = int_precision
+        self.file_indices = []
+        self.data_range = (None, None)
+        self.complete_bbox_points = None
+        self.complete_labels = None
+        self.singl_sem_classes = singl_sem_classes
+        self.multi_sem_classes = None
+        self.translate_vector = translate_vector
+        self.rotation_matrix = rotation_matrix
+        
+        
+    def setup(self):
+        for filename in os.listdir(self.data_path):
+            if filename.startswith("bounding_box_3d_") and filename.endswith(".npy"):
+                suffix = filename.split("_")[-1].split(".")[0]
+                self.int_precision = max(self.int_precision, len(suffix))
+                self.file_indices.append(int(suffix))
+        
+        if self.file_indices:
+            self.data_range = (min(self.file_indices), max(self.file_indices))
+        
+        self.complete_bbox_points = np.empty(self.data_range[1] - self.data_range[0] + 1, dtype=list)
+        self.complete_labels = np.empty(self.data_range[1] - self.data_range[0] + 1, dtype=dict)
+        
+    def load_labels(self):
+        for i in range(self.data_range[0], self.data_range[1] + 1):
+            index_str = f"{i:0{self.int_precision}d}"
+            labels_file_path = os.path.join(self.data_path, f"bounding_box_3d_labels_{index_str}.json")
+            if os.path.exists(labels_file_path):
+                with open(labels_file_path, 'r') as labels_file:
+                    labels_data = json.load(labels_file)
+                    self.complete_labels[i - self.data_range[0]] = labels_data
+            else:
+                self.complete_labels[i - self.data_range[0]] = None
+                
+        # find all unique labels
+        all_labels = set()
+        for labels in self.complete_labels:
+            if labels:
+                for label in labels.values():
+                    all_labels.add(label['class'])
+
+        self.multi_sem_classes = list(all_labels - set(self.singl_sem_classes))
+                
+    def load_data(self):
+        for i in range(self.data_range[0], self.data_range[1] + 1):
+            index_str = f"{i:0{self.int_precision}d}"
+            bbox3d_data_file_path = os.path.join(self.data_path, f"bounding_box_3d_{index_str}.npy")
+            bbox3d_data = np.load(bbox3d_data_file_path)
+            local_bbox_points = []
+            
+            single_sem_points = {}
+            for class_name in self.singl_sem_classes:
+                single_sem_points[class_name] = [] # list of points
+                
+            multi_sem_points = {}
+            for class_name in self.multi_sem_classes:
+                multi_sem_points[class_name] = [] # list of list of points
+            
+            for row in bbox3d_data:
+                class_id = str(row[0])
+                class_name = self.complete_labels[i - self.data_range[0]][class_id]['class']
+                x_min = row[1]
+                y_min = row[2]
+                x_max = row[3]
+                y_max = row[4]
+                z_min = row[5]
+                z_max = row[6]
+                points = [
+                    [x_min, y_min, z_min],
+                    [x_min, y_min, z_max],
+                    [x_min, y_max, z_min],
+                    [x_min, y_max, z_max],
+                    [x_max, y_min, z_min],
+                    [x_max, y_min, z_max],
+                    [x_max, y_max, z_min],
+                    [x_max, y_max, z_max]
+                ]
+                transform = row[7].T
+                
+                pcd = o3d.geometry.PointCloud()
+                pcd.points = o3d.utility.Vector3dVector(np.array(points).astype(np.float64))
+                pcd.transform(np.array(transform).astype(np.float64))
+                pcd.translate(self.translate_vector)
+                pcd.rotate(self.rotation_matrix, center=(0, 0, 0))
+                points = np.asarray(pcd.points)
+                
+                if class_name in self.singl_sem_classes:
+                    single_sem_points[class_name].extend(points)
+                elif class_name in self.multi_sem_classes:
+                    multi_sem_points[class_name].append(points)
+                    
+            for class_name, points in single_sem_points.items():
+                local_bbox_points.append(points)
+            
+            for class_name, points_list in multi_sem_points.items():
+                local_bbox_points.extend(points_list)
+            
+            self.complete_bbox_points[i - self.data_range[0]] = local_bbox_points
+            
--- synthetic_data_viz/dev_viz.py
+++ synthetic_data_viz/dev_viz.py
@@ -5,48 +5,13 @@ import open3d as o3d
 import time
 
 from common.o3d_visualizer import O3dVisualizer
+from common.data_loaders.synthetic_bbox import SyntheticBbox
 
 ### Loading data
 
 synthetic_data_path = '/home/digi2/colino_dir/gen_data_ground_truth/figure8_transporter_empty-30fps_800frames-rec1'
-
-# Initialize a list to store the numeric parts of the filenames
-file_indices = []
-int_precision = 0
-
-# Iterate over the files in the directory
-for filename in os.listdir(synthetic_data_path):
-    if filename.startswith("bounding_box_3d_") and filename.endswith(".npy"):
-        # Extract the numeric part of the filename and convert it to an integer
-        suffix = filename.split("_")[-1].split(".")[0]
-        int_precision = max(int_precision, len(suffix))
-        file_indices.append(int(suffix))
-
-# Calculate the range of the data points
-if file_indices:
-    data_range = (min(file_indices), max(file_indices))
-else:
-    data_range = (None, None)
-
-print(f"Range of data points: {data_range}")
-
-if data_range[0] is not None and data_range[1] is not None:
-    for i in range(data_range[0], data_range[1] + 1):
-        # Format the index with leading zeros based on the detected precision
-        index_str = f"{i:0{int_precision}d}"
-        
-        # Construct the file paths using the formatted index
-        labels_file_path = os.path.join(synthetic_data_path, f"bounding_box_3d_labels_{index_str}.json")
-        
-        if os.path.exists(labels_file_path):
-            with open(labels_file_path, 'r') as labels_file:
-                labels_data = json.load(labels_file)
-else:
-    print("No data points found")
-    exit(1)
-            
-
-### Transformation matrix according to the camera coordinate system
+    
+### Transformations according to the camera coordinate system
 """
 Global Position: (-10.000000000540384, 5.0018717613276635, 0.21277073854091316)
 Rotation Matrix:
@@ -79,100 +44,35 @@ rotation_matrix = np.array([
 # Define the translation vector (3x1)
 translation_vector = -camera_position
 rotation_matrix = camera_rotation @ rotation_x_minus_90.T @ rotation_matrix
-
-# Create the 4x4 transformation matrix
-transformation_matrix = np.eye(4)
-transformation_matrix[:3, :3] = rotation_matrix
-transformation_matrix[:3, 3] = translation_vector
-
             
-multi_sem_classes = ['swetfloorsign']
 singl_sem_classes = ['transporter1_mesh']
+
+synthetic_bbox = SyntheticBbox(synthetic_data_path, singl_sem_classes=singl_sem_classes,
+                                translate_vector=translation_vector, rotation_matrix=rotation_matrix)
+synthetic_bbox.setup()
+synthetic_bbox.load_labels()
+synthetic_bbox.load_data()
+
+data_range = synthetic_bbox.data_range
 
 # initiate np array with size of data_range
 complete_pcds = np.empty(data_range[1] - data_range[0] + 1, dtype=list)
 complete_bboxes = np.empty(data_range[1] - data_range[0] + 1, dtype=list)
     
-
 for i in range(data_range[0], data_range[1] + 1):
-    # Format the index with leading zeros based on the detected precision
-    index_str = f"{i:0{int_precision}d}"
+    complete_pcds[i - data_range[0]] = []
+    complete_bboxes[i - data_range[0]] = []
 
-    # Construct the file paths using the formatted index
-    labels_file_path = os.path.join(synthetic_data_path, f"bounding_box_3d_labels_{index_str}.json")
-
-    if os.path.exists(labels_file_path):
-        with open(labels_file_path, 'r') as labels_file:
-            labels_data = json.load(labels_file)
-            
-    bbox3d_data_file_path = os.path.join(synthetic_data_path, f"bounding_box_3d_{index_str}.npy")
-    bbox3d_data = np.load(bbox3d_data_file_path)
-    
-    pcd_geometries = []
-    
-    singl_sem_classes_pcds = {}
-    
-    for class_name in singl_sem_classes:
-        singl_sem_classes_pcds[class_name] = o3d.geometry.PointCloud()
-    
-    multi_sem_classes_pcds = {}
-    
-    for class_name in multi_sem_classes:
-        multi_sem_classes_pcds[class_name] = []
-    
-    for row in bbox3d_data:
+    for j, bbox_points in enumerate(synthetic_bbox.complete_bbox_points[i - data_range[0]]):
         
-        class_id = str(row[0])
-        class_name = labels_data[class_id]['class']
-        
-        x_min = row[1]
-        y_min = row[2]
-        x_max = row[3]
-        y_max = row[4]
-        z_min = row[5]
-        z_max = row[6]
-        
-        points = [
-            [x_min, y_min, z_min],
-            [x_min, y_min, z_max],
-            [x_min, y_max, z_min],
-            [x_min, y_max, z_max],
-            [x_max, y_min, z_min],
-            [x_max, y_min, z_max],
-            [x_max, y_max, z_min],
-            [x_max, y_max, z_max]
-        ]
-        
-        transform = row[7].T
-        
+        bbox_points = np.array(bbox_points).astype(np.float64)
         
         pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(np.array(points).astype(np.float64))
-        pcd.transform(np.array(transform).astype(np.float64))
-        #
-        pcd.translate(translation_vector)
-        pcd.rotate(rotation_matrix, center=(0, 0, 0))
-            
-        if class_name in singl_sem_classes:
-            singl_sem_classes_pcds[class_name].points.extend(pcd.points)
-        elif class_name in multi_sem_classes:
-            multi_sem_classes_pcds[class_name].append(pcd)
-            
-    for class_name in singl_sem_classes:
-        pcd_geometries.append(singl_sem_classes_pcds[class_name])
+        pcd.points = o3d.utility.Vector3dVector(np.array(bbox_points).astype(np.float64))
+        complete_pcds[i - data_range[0]].append(pcd)
         
-    for class_name in multi_sem_classes:
-        pcd_geometries.extend(multi_sem_classes_pcds[class_name])
-        
-    bboxes_geometries = []
-    
-    for pcd in pcd_geometries:
-        bboxes_geometries.append(pcd.get_axis_aligned_bounding_box())
-        
-        
-    complete_pcds[i - data_range[0]] = pcd_geometries
-    complete_bboxes[i - data_range[0]] = bboxes_geometries
-            
+        bbox = pcd.get_axis_aligned_bounding_box()
+        complete_bboxes[i - data_range[0]].append(bbox)
         
 ### visualization
 
