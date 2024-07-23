dc769692337c163972730e900581870b210d761f
FranciscoMColino <francisco.m.colino@gmail.com>
2024-07-23 18:01:24 +0100

    Add data loader for detection 3d bboxes

---
+++ common/data_loaders/detection_bbox.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+
+class DetectionBbox():
+    def __init__(self, data_path, int_precision=5):
+        self.data_path = data_path
+        self.int_precision = int_precision
+        self.file_indices = []
+        self.data_range = (None, None)
+        self.complete_bbox_points = None
+        self.complete_timestamps = None
+        
+    def setup(self):
+        for filename in os.listdir(self.data_path):
+            if filename.startswith("detected_bbox3d_") and filename.endswith(".npy"):
+                suffix = filename.split("_")[-1].split(".")[0]
+                self.int_precision = max(self.int_precision, len(suffix))
+                self.file_indices.append(int(suffix))
+        
+        if self.file_indices:
+            self.data_range = (min(self.file_indices), max(self.file_indices))
+        
+        self.complete_bbox_points = np.empty(self.data_range[1] - self.data_range[0] + 1, dtype=list)
+        self.complete_timestamps = np.empty(self.data_range[1] - self.data_range[0] + 1, dtype=dict)
+        
+    def load_data(self):
+        for i in range(self.data_range[0], self.data_range[1] + 1):
+            index_str = f"{i:0{self.int_precision}d}"
+            bboxes_file_path = os.path.join(self.data_path, f"detected_bbox3d_{index_str}.npy")
+            bboxes_data_np = np.load(bboxes_file_path, allow_pickle=True)
+            detections_data = bboxes_data_np['detections']
+            local_bbox_points = []
+            for detections in detections_data:
+                id = detections['id'] # 0, not used
+                label = detections['label'] # 1, not used
+                bbox_points = detections['points'].astype(np.float64) # 2
+                bbox_centroid = detections['centroid'].astype(np.float64) # 3, not used
+                bbox_transform = detections['transform'].astype(np.float64) # 4, not used
+                local_bbox_points.append(bbox_points)
+                
+            self.complete_bbox_points[i - self.data_range[0]] = local_bbox_points
+            self.complete_timestamps[i - self.data_range[0]] = {
+                'seconds': bboxes_data_np['seconds'],
+                'nanoseconds': bboxes_data_np['nanoseconds']
+            }
+        
+    
+        
---
+++ detection_data_viz/dev_detection_viz.py
@@ -0,0 +1,72 @@
+import os
+import json
+import numpy as np
+import open3d as o3d
+import time
+
+from common.o3d_visualizer import O3dVisualizer
+from common.data_loaders.detection_bbox import DetectionBbox
+
+detection_data_path = '/home/digi2/colino_dir/detection_data/ermis_detection/detection_res_2'
+
+detection_bbox = DetectionBbox(detection_data_path, int_precision=5)
+detection_bbox.setup()
+detection_bbox.load_data()
+
+data_range = detection_bbox.data_range
+
+complete_pcds = np.empty(data_range[1] - data_range[0] + 1, dtype=list)
+complete_bboxes = np.empty(data_range[1] - data_range[0] + 1, dtype=list)
+complete_timestamps = np.empty(data_range[1] - data_range[0] + 1, dtype=dict)
+
+for i in range(data_range[0], data_range[1] + 1):
+    
+    local_pcds = []
+    local_bboxes = []
+    
+    for bbox_points in detection_bbox.complete_bbox_points[i - data_range[0]]:
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(bbox_points)
+        local_pcds.append(pcd)
+        
+        bbox = o3d.geometry.OrientedBoundingBox.create_from_points(pcd.points)
+        local_bboxes.append(bbox)
+        
+    complete_pcds[i - data_range[0]] = local_pcds
+    complete_bboxes[i - data_range[0]] = local_bboxes
+    complete_timestamps[i - data_range[0]] = detection_bbox.complete_timestamps[i - data_range[0]]
+    
+# estimate fps from first and last timestamps and number of frames
+if complete_timestamps.all():
+    start_time = complete_timestamps[0]
+    end_time = complete_timestamps[-1]
+    start_seconds = start_time['seconds'] + start_time['nanoseconds'] / 1e9
+    end_seconds = end_time['seconds'] + end_time['nanoseconds'] / 1e9
+    num_frames = len(complete_timestamps)
+    fps = num_frames / (end_seconds - start_seconds)
+print(f"Estimated FPS: {fps}")
+    
+
+### visualization
+
+o3d_visualizer = O3dVisualizer()
+o3d_visualizer.setup()
+
+for i in range(data_range[0], data_range[1] + 1):
+    o3d_visualizer.reset()
+    
+    for geometry in complete_pcds[i - data_range[0]]:
+        o3d_visualizer.vis.add_geometry(geometry, reset_bounding_box=False)
+        
+    for geometry in complete_bboxes[i - data_range[0]]:
+        geometry.color = (1, 0, 0)
+        o3d_visualizer.vis.add_geometry(geometry, reset_bounding_box=False)
+    
+    o3d_visualizer.render()
+    
+    time.sleep(1/fps)
+        
+
+o3d_visualizer.vis.destroy_window()
+    
+    
