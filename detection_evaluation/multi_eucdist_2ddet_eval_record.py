793de4d973976851bdbc314983a47140bedaf60a
FranciscoMColino <francisco.m.colino@gmail.com>
2024-08-02 14:30:27 +0100

    Add results recording to evaluation scripts

--- detection_evaluation/multi_eucdist_2ddet_eval_record.py
+++ detection_evaluation/multi_eucdist_2ddet_eval_record.py
@@ -3,16 +3,55 @@ import argparse
 import yaml
 import numpy as np
 import math
+import datetime
 
 from detection_evaluation.eucdist_2ddet_evaluation import eucdist_evaluate_2ddet_data
 
-def pretty_print_evaluation_results(results):
-    print("\nFinal Evaluation Results:")
+def pretty_str_evaluation_results(results):
+    result_str = "\nFinal Evaluation Results:\n"
     for name in results.dtype.names:
         values = results[name]
-        print(f"{name.capitalize()}: {['{:.2f}'.format(val) for val in values]}")
+        result_str += f"{name.capitalize()}: {['{:.2f}'.format(val) for val in values]}\n"
+    return result_str
 
-def multi_eval_record(config_data, verbose=False):
+def print_log(message, log_file=None):
+    print(message)
+    if log_file is not None:
+        with open(log_file, 'a') as f:
+            f.write(message + '\n')
+
+def multi_eval_record(config_data):
+
+    results_recording_enabled = config_data['results_recording']['enabled']
+    results_recording_output_dir = config_data['results_recording']['output_dir']
+
+    logging_file = None
+
+    if results_recording_enabled:
+        if not os.path.exists(results_recording_output_dir):
+            os.makedirs(results_recording_output_dir)
+        elif not os.path.isdir(results_recording_output_dir):
+            raise NotADirectoryError(f"Results recording output directory {results_recording_output_dir} is not a directory")
+        else:
+            raise FileExistsError(f"Results recording output directory {results_recording_output_dir} already exists")
+        
+        logging_file = os.path.join(results_recording_output_dir, 'log.txt')
+        config_copy_file = os.path.join(results_recording_output_dir, 'config_copy.yaml')
+        metadata_file = os.path.join(results_recording_output_dir, 'metadata.yaml')
+
+        with open(config_copy_file, 'w') as file:
+            yaml.dump(config_data, file)
+
+        with open(metadata_file, 'w') as file:
+            metadata = {
+                'date': datetime.datetime.now().strftime("%Y-%m-%d"),
+                'time': datetime.datetime.now().strftime("%H:%M:%S")
+            }
+            yaml.dump(metadata, file)
+        
+        print_log(f"Results recording enabled. Results will be saved to: {results_recording_output_dir}", logging_file)
+    else:
+        print_log("Results recording disabled", logging_file)
 
     evaluation_data = config_data['evaluation_data']
 
@@ -22,19 +61,22 @@ def multi_eval_record(config_data, verbose=False):
     camera_position = config_data['camera_position']
     camera_rotation = config_data['camera_rotation']
 
-    print(f"Lenght of evaluation data: {len(evaluation_data)}")
+    evaluation_data_name_ids = [data['name_id'] for data in evaluation_data]
+
+    print_log(f"\nEvaluating data: {evaluation_data_name_ids}", logging_file)
 
     sims_results = []
 
     for data in evaluation_data:
-        print(f"\nEvaluating data: {data['name_id']}")
+        print_log(f"\nEvaluating data: {data['name_id']}", logging_file)
         data['euclidean_distance_thresholds'] = euclidean_distance_thresholds
         data['camera_position'] = camera_position
         data['camera_rotation'] = camera_rotation
         data['geometry_mode'] = geometry_mode
         
         results = eucdist_evaluate_2ddet_data(data, verbose=False)
-        pretty_print_evaluation_results(results)
+        results_str = pretty_str_evaluation_results(results)
+        print_log(results_str, logging_file)
 
         sims_results.append(results)
 
@@ -82,8 +124,8 @@ def multi_eval_record(config_data, verbose=False):
         average_scale_error
     ), dtype=evaluation_results_dtype)
 
-    print(f"\n Mean Average Results:")
-    pretty_print_evaluation_results(final_evaluation_results)
+    print_log(f"\n Mean Average Results:", logging_file)
+    print_log(pretty_str_evaluation_results(final_evaluation_results), logging_file)
 
 
 def main():
@@ -98,7 +140,7 @@ def main():
     with open(args.config_file, 'r') as file:
         config_data = yaml.safe_load(file)
 
-    multi_eval_record(config_data, verbose=True)
+    multi_eval_record(config_data)
 
 if __name__ == "__main__":
     main()
